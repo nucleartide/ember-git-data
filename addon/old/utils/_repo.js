2b345e3ccb0806c24a6098dc11c2808faf4b6201
Jason Tu <jasontu4@gmail.com>
2016-07-29 18:14:10 -0400

    remove comment

--- addon/old/utils/_repo.js
+++ addon/old/utils/_repo.js
@@ -11,12 +11,6 @@ const {
   merge,
 } = Ember
 
-/**
- * TODO: generators "just work" in ember...if i recall
- * ember-concurrency did some things to optimize file size?
- * something about not including the polyfill.
- * TODO: github rate limit
- */
 export default class Repo {
   constructor(githubAjax, owner, repo, branch) {
     assert('must pass in githubAjax', githubAjax)
