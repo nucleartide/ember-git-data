aeb403ef72aec55aec7753b1a8ab2002ecdf972e
Jason Tu <jasontu4@gmail.com>
2016-08-09 18:34:06 -0400

    fix import since node_modules is flat in npm 3

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ var Funnel = require('broccoli-funnel')
 module.exports = {
   name: 'ember-git-data',
 
+  treeForVendor: function(tree) {
+    var packagePath = path.dirname(require.resolve('git-data'))
+    var packageTree = new Funnel(this.treeGenerator(packagePath), {
+      srcDir: '/',
+      destDir: 'git-data',
+    })
+
+    var nodes = tree ? [tree, packageTree] : [packageTree]
+    return mergeTrees(nodes)
+  },
+
   included: function(app) {
-    app.import('vendor/git-data.js')
+    app.import('vendor/git-data/build/index.js')
     app.import('vendor/shim.js')
   },
 }
